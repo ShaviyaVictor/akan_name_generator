172c946107d2ef6285bacf02c4d848bc5567a939
ShaviyaVictor <shaviyavictor@gmail.com>
2021-11-13 05:23:48 +0300

    link bootstrap and my custom js as well partition the page into sections

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+//let add = function(num1, num2){
+  return num1 / num2;
+}
+let num1 = parseInt(prompt('Enter a number:'));
+let num2 = parseInt(prompt('Enter another number:'));
+let results = add(num1, num2);
+alert(results);
