8bd36d22c2d0a1ac1dab39335464d53131cd280a
ShaviyaVictor <shaviyavictor@gmail.com>
2021-11-17 04:31:37 +0300

    Create the variables for the js function

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,15 @@
+//testing to see if I've linked them properly
 //let add = function(num1, num2){
-  return num1 / num2;
-}
-let num1 = parseInt(prompt('Enter a number:'));
-let num2 = parseInt(prompt('Enter another number:'));
-let results = add(num1, num2);
-alert(results);
+//  return num1 / num2;
+//}
+//let num1 = parseInt(prompt('Enter a number:'));
+//let num2 = parseInt(prompt('Enter another number:'));
+//let results = add(num1, num2);
+//alert(results);
+
+//      creating variables for the days of the week and the gender names
+let masculineNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+let feminineNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurdsday', 'Friday', 'Saturday']
+
+
